f0000be696461867fadc3706b06ac26a0ea51389
Mohottige Chamil Asanka <mohottige.chamilasan@petronas.com>
2024-01-18 16:18:27 +0700

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https:localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = ["Jonh", "Jane", "Mark", "Emily", "David"];
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DefficultyId { get; set; }
+        public required Difficulty Difficulty { get; set; }
+        public Guid RegionId { get; set; }
+        public required Region Region { get; set; }
+    }
+}
