6b1749a13514b2e004f3ada9f7b80a2414a382a0
Mohottige Chamil Asanka <mohottige.chamilasan@petronas.com>
2024-02-12 15:26:42 +0700

    Filter,Sorting,Pagination in ASP.Net Core Web API

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -35,11 +35,16 @@ namespace NZWalks.API.Controllers
         }
 
         // Get Walks
-        // GET: /api/walks
+        // GET: /api/walks?FilterOn=Name&FilterQuery=Track&sortBy=Name&isAscending=true&pageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery,
+            [FromQuery] string? sortBy, [FromQuery] bool? isAscending,
+            [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000
+            )
         {
-            var walksDomainModel = await walkRepository.GetAllAsync();
+            var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery,
+                sortBy, isAscending ?? true, 
+                pageNumber, pageSize);
 
             return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
         }
